ec34e881f2d5ac68e8728b2c3b3269a0a9dca4d4
Matthew Grillo <grillom@bishopireton.org>
2019-05-01 11:05:48 -0400

    Added TODO.txt

--- ShapeTesterTK.java
+++ ShapeTesterTK.java
@@ -2,69 +2,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShapeTesterTK {
-private final static int MINSIZE = 10;
-private final static int MAXSIZE = 100;
+   private final static int MINSIZE = 10;
+   private final static int MAXSIZE = 100;
 
- private List<Shape> shapes;
+   private List<Shape> shapes;
  
- ShapeTesterTK() {
-   shapes = new ArrayList<Shape>(MAXSIZE);
+   ShapeTesterTK() {
+      shapes = new ArrayList<Shape>(MAXSIZE);
    }
    
- private double randNum() {
-   return Math.random() * (MAXSIZE - MINSIZE) + MINSIZE;
- }
+   private double randNum() {
+      return Math.random() * (MAXSIZE - MINSIZE) + MINSIZE;
+   }
   
-  private void addCircle() {
-    shapes.add(new Circle(randNum()));
-  }
+   private void addCircle() {
+      shapes.add(new Circle(randNum()));
+   }
   
-  private void addRectangle() {
-    shapes.add(new Rectangle(randNum(), randNum()));
-  }
+   private void addRectangle() {
+      shapes.add(new Rectangle(randNum(), randNum()));
+   }
   
-  private void addSquare() {
-    shapes.add(new Square(randNum()));
-  }
+   private void addSquare() {
+      shapes.add(new Square(randNum()));
+   }
 
-  private void addTriangle() {
-   int rand = (int) (Math.random() * 4);
-   Shape triangle = null;
+   private void addTriangle() {
+      int rand = (int) (Math.random() * 4);
+      Shape triangle = null;
    
-    switch (rand) {
-      case 0: triangle = new EquilateralTriangle(randNum()); break;
-     // case 1: triangle = new Scalene(randNum(), randNum(), randNum()); break;
-     // case 2: triangle = new Isoceles(randNum(), randNum()); break;
-      case 3: triangle = new RightTriangle(randNum(), randNum()); break;
-   } 
-   if (! (triangle == null)) shapes.add(triangle);
- }
+      switch (rand) {
+         case 0: triangle = new EquilateralTriangle(randNum()); 
+            break;
+      // case 1: triangle = new Scalene(randNum(), randNum(), randNum()); break;
+      // case 2: triangle = new Isoceles(randNum(), randNum()); break;
+         case 3: triangle = new RightTriangle(randNum(), randNum()); 
+            break;
+      } 
+      if (! (triangle == null)) shapes.add(triangle);
+   }
   
-  private void addRandomShape() {
-    int rand = (int) (Math.random() * 7);
-    switch (rand) {
-      case 0: addCircle(); break;
-      case 1: addSquare(); break;
-      case 2: addRectangle(); break;
-      case 3: addTriangle(); break;
-      case 4: addTriangle(); break;
-      case 5: addTriangle(); break;
-      case 6: addTriangle(); break;
+   private void addRandomShape() {
+      int rand = (int) (Math.random() * 7);
+      switch (rand) {
+         case 0: addCircle(); 
+            break;
+         case 1: addSquare(); 
+            break;
+         case 2: addRectangle(); 
+            break;
+         case 3: addTriangle(); 
+            break;
+         case 4: addTriangle(); 
+            break;
+         case 5: addTriangle(); 
+            break;
+         case 6: addTriangle(); 
+            break;
       }
-}
+   }
 
-public String toString() {
-  String result = "";
-  for (Shape s : shapes) 
-    result += s + "\n";
-  return result;
-}  
+   public String toString() {
+      String result = "";
+      for (Shape s : shapes) 
+         result += s + "\n";
+      return result;
+   }  
 
-public static void main(String[] args) {
+  public void legalExamples() {
+    for (Shape s : shapes) {
+      if (s instanceof Circle) {
+        System.out.printf("Circumference: %s\n", ((Circle)s).circumference());
+        }
+       }
+       
+        
+  }
   
-  ShapeTesterTK st = new ShapeTesterTK();
+  public static void compilerErrorExamples() {
   
-  for (int i = 0; i < MAXSIZE; i++) st.addRandomShape();
-  System.out.println(st);
-}
+  }
+  
+  public static void runtimeErrorExamples() {
+  
+  }
+
+
+   public static void main(String[] args) {
+   
+      ShapeTesterTK st = new ShapeTesterTK();
+   
+      for (int i = 0; i < MAXSIZE; i++) st.addRandomShape();
+      System.out.println(st);
+      
+      st.legalExamples();
+      st.compilerErrorExamples();
+      st.runtimeErrorExamples();
+   }
 }
--- Square.java
+++ Square.java
@@ -1,32 +1,31 @@
 public class Square extends Rectangle {
 
-public static final String type = "Square";
+   public static final String TYPE = "Square";
 
-Square() {
-  super(1, 1);
- }
+   Square() {
+      super(1, 1);
+   }
  
- Square(double s) {
-   super(s,s);
-    }
+   Square(double s) {
+      super(s,s);
+   }
  
- public double getSide() {
-   return super.getLength();
+   public double getSide() {
+      return super.getLength();
    }
    
-   
    public void setSide(double s) {
-     super.setLength(s);
-     super.setWidth(s);
+      super.setLength(s);
+      super.setWidth(s);
    }
    
    public double perimeter() {
-     return 4 * super.getLength();
+      return 4 * super.getLength();
    }
    
    public String toString() {
-     return String.format("%s: side: %.2f, area: %2f, perimeter: %.2f", 
-                              type, super.getLength(), area(), perimeter());
+      return String.format("%s: side: %.2f, area: %2f, perimeter: %.2f", 
+                              TYPE, super.getLength(), area(), perimeter());
     
    }
  
