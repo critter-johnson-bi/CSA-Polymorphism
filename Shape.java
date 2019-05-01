a7b928670c1bc0e0ec4c92855e990b5cc04b32ff
Matthew Grillo <grillom@bishopireton.org>
2019-05-01 09:14:56 -0400

    The new shape file

---
+++ Shape.java
@@ -0,0 +1,15 @@
+// All Shapes Need to implement the shape interface
+
+// Squares should extend rectangles
+// 
+//
+// What do all shapes need?
+// Area
+// Anything else?
+// 
+//
+public interface Shape {
+
+  public double area();
+
+}
