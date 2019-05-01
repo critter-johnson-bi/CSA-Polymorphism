2a4f8321858f15669a73c901e4bfcc59911ea261
Matthew Grillo <grillom@bishopireton.org>
2019-05-01 09:32:41 -0400

    fixed Critter's typo

--- Square.java
+++ Square.java
@@ -1,5 +1,6 @@
 public class Square extends Rectangle {
 
+public static final String type = "Square";
 
 Square() {
   super(1, 1);
@@ -9,9 +10,20 @@ Square() {
    return super.getLength();
    }
    
+   
    public void setSide(double s) {
      super.setLength(s);
      super.setWidth(s);
    }
+   
+   public double perimeter() {
+     return 4 * super.getLength();
+   }
+   
+   public String toString() {
+     return String.format("%s: side: %.sf, area: %2f, perimeter: %.sf", 
+                              type, super.getLength(), area(), perimeter());
+    
    }
  
+}
