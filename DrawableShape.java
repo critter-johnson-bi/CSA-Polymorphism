4370b9fe4449c85def7922d2f4730cf8562ec5df
KsMomOf2 <kellyt@bishopireton.org>
2019-05-03 12:00:41 -0400

    Updated Drawable

--- DrawableShape.java
+++ DrawableShape.java
@@ -1,15 +1,56 @@
+import java.awt.Point;
+import java.awt.Graphics;
+import java.awt.Color;
+
 public abstract class DrawableShape extends Shape {
   private static final int MIN_SPEED = 5;
   private static final int MAX_SPEED = 20;
-  
+ 
+  private Point location;
+  private Color lineColor;
+  private Color fillColor;
+ 
+
   private boolean draw;
   private double speed;
-  
+    abstract void drawMe(Graphics g);
+
  DrawableShape() {
     int option = (int) Math.random() * 3;
     draw = option < 2;
     speed =  getRandomSpeed();
      }
+ 
+  public Point getLocation() {
+    return location;
+  }
+  
+  public Color getLineColor() {
+    return lineColor;
+  }
+  
+  public Color getFillColor() {
+    return fillColor;
+  }
+  
+  public void setLineColor(Color c) {
+    lineColor = c;
+  }
+  
+  public void setFillColor(Color c) {
+    fillColor = c;
+  }
+  
+  public void setLocation(int x, int y) {
+    if (location == null) location = new Point();
+    location.setLocation(x, y);
+  }
+  
+  public void setLocation(Point p) {
+    if (location == null) location = new Point();
+    location.setLocation(p);
+  }
+  
   
   public boolean getDraw() {
     return draw;
