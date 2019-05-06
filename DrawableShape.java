a9af54f3b46bbb4cb41743a3bf0da1c310b8c7d1
critter-johnson-bi <christohper.johnson@bishopireton.org>
2019-05-06 13:13:54 -0400

    fixed Mrs. Kelly's horrible spacing

--- DrawableShape.java
+++ DrawableShape.java
@@ -3,37 +3,37 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public interface DrawableShape {
-  static final int MIN_SPEED = 5;
-  static final int MAX_SPEED = 20;
- 
+   static final int MIN_SPEED = 5;
+   static final int MAX_SPEED = 20;
+  
 /*
-  private Point location;
-  private Color lineColor;
-  private Color fillColor;
-  private boolean draw;
-  private double speed;
+   private Point location;
+   private Color lineColor;
+   private Color fillColor;
+   private boolean draw;
+   private double speed;
 */
-    void drawMe(Graphics g);
+   void drawMe(Graphics g);
 
    Point getLocation();
-   
-  Color getLineColor();
+    
+   Color getLineColor();
 
-  Color getFillColor();
- 
-  void setLineColor(Color c);
+   Color getFillColor();
   
-  void setFillColor(Color c);
-  
-  void setLocation(int x, int y);
-  
-  void setLocation(Point p);
-  
-  boolean getDraw();
-  
-  double getSpeed();
-  
-  void setDraw(boolean b);
-  
-  void setSpeed(double s);
-    }
+   void setLineColor(Color c);
+   
+   void setFillColor(Color c);
+   
+   void setLocation(int x, int y);
+   
+   void setLocation(Point p);
+   
+   boolean getDraw();
+   
+   double getSpeed();
+   
+   void setDraw(boolean b);
+   
+   void setSpeed(double s);
+}
