80ae5709e9d6d6fb55569467a3a8849f2e5394ad
Matthew Grillo <grillom@bishopireton.org>
2019-05-06 12:49:11 -0400

    change drawable shape to interface

--- DrawableShape.java
+++ DrawableShape.java
@@ -2,79 +2,41 @@ import java.awt.Point;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public abstract class DrawableShape extends Shape {
-  private static final int MIN_SPEED = 5;
-  private static final int MAX_SPEED = 20;
+public interface DrawableShape {
+  static final int MIN_SPEED = 5;
+  static final int MAX_SPEED = 20;
  
+/*
   private Point location;
   private Color lineColor;
   private Color fillColor;
- 
+
 
   private boolean draw;
   private double speed;
+
+*/
     abstract void drawMe(Graphics g);
 
- DrawableShape() {
-    int option = (int) Math.random() * 3;
-    draw = option < 2;
-    speed =  getRandomSpeed();
-     }
+   Point getLocation();
+   
+  Color getLineColor();
+
+  Color getFillColor();
  
-  public Point getLocation() {
-    return location;
-  }
+  void setLineColor(Color c);
   
-  public Color getLineColor() {
-    return lineColor;
-  }
+  void setFillColor(Color c);
   
-  public Color getFillColor() {
-    return fillColor;
-  }
+  void setLocation(int x, int y);
   
-  public void setLineColor(Color c) {
-    lineColor = c;
-  }
+  void setLocation(Point p);
   
-  public void setFillColor(Color c) {
-    fillColor = c;
-  }
+  boolean getDraw();
   
-  public void setLocation(int x, int y) {
-    if (location == null) location = new Point();
-    location.setLocation(x, y);
-  }
+  double getSpeed();
   
-  public void setLocation(Point p) {
-    if (location == null) location = new Point();
-    location.setLocation(p);
-  }
+  void setDraw(boolean b);
   
-  
-  public boolean getDraw() {
-    return draw;
-  }
-  
-  public double getSpeed() {
-    return speed;
-  } 
-  
-  public static double getRandomSpeed() {
-    return Math.random() * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
-  }  
- 
-  public void setDraw(boolean b) {
-    draw = b;
-  }
-  
-  public void setSpeed(double s) {
-    speed = s;
-  }
-  
-  public String toString() {
-    String result = super.toString();
-    result += String.format(" draw: %b, speed: %,.2f", draw, speed); 
-    return result;
-  }
-  }
+  void setSpeed(double s);
+    }
