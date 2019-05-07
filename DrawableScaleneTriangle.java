d35b9a83eda4fb09dc2b68cc91d20c5d02b62641
critter-johnson-bi <43154693+critter-johnson-bi@users.noreply.github.com>
2019-05-06 23:28:29 -0400

    made scalene drawable

--- DrawableScaleneTriangle.java
+++ DrawableScaleneTriangle.java
@@ -3,7 +3,82 @@
 *  Date: 5/6/19
 */
 
+import java.awt.Point;
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class DrawableScaleneTriangle extends ScaleneTriangle implements DrawableShape
 {
+   private Point location;
+   private Color lineColor;
+   private Color fillColor;
+   private boolean draw;
+   private double speed;
    
+   public DrawableScaleneTriangle(double a, double b, double c, Color lineColor, Color fillColor)
+   {
+      super(a, b, c);
+      this.lineColor = lineColor;
+      this.fillColor = fillColor;
+   }
+   
+   public void drawMe(Graphics g)
+   {
+      
+   }
+   
+   public Point getLocation()
+   {
+      return location;
+   }
+    
+   public Color getLineColor()
+   {
+      return lineColor;
+   }
+
+   public Color getFillColor()
+   {
+      return fillColor;
+   }
+  
+   public void setLineColor(Color c)
+   {
+      this.lineColor = c;
+   }
+   
+   public void setFillColor(Color c)
+   {
+      this.fillColor = c;
+   }
+   
+   public void setLocation(int x, int y)
+   {
+      this.location = new Point(x, y);
+   }
+   
+   public void setLocation(Point p)
+   {
+      this.location = p;
+   }
+   
+   public boolean getDraw()
+   {
+      return draw;
+   }
+   
+   public double getSpeed()
+   {
+      return speed;
+   }
+   
+   public void setDraw(boolean b)
+   {
+      this.draw = b;
+   }
+   
+   public void setSpeed(double s)
+   {
+      this.speed = s;
+   }
 }
