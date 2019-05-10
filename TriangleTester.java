1ebfc5bad7b1e868fec911d550693d4aa23c0133
critter-johnson-bi <43154693+critter-johnson-bi@users.noreply.github.com>
2019-05-09 21:20:54 -0400

    updated triangle tester

--- TriangleTester.java
+++ TriangleTester.java
@@ -5,5 +5,13 @@
 
 public class TriangleTester
 {
-   
+   public static void main(String[] args)
+   {
+      // tests right triangles
+      Triangle rt = new RightTriangle(3, 4);
+      RightTriangle rt2 = new RightTriangle(5, 12);
+      
+      System.out.println(rt.getArea());
+      System.out.println(rt2.getArea());
+   }
 }
